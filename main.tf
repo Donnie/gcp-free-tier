c6fb32764a50993790e81e2155c2ef51e2f3b756
Donnie <donnie.ashok@moonfare.com>
2022-08-12 13:56:04 +0200

    Working setup

---
+++ main.tf
@@ -0,0 +1,48 @@
+# Define Terraform provider
+terraform {
+  required_version = "~> 1.2.5"
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.31.0" # pinning version
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_name
+  zone        = var.zone_free_south_carolina
+}
+
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = var.machine_type_free
+  zone         = var.zone_free_south_carolina
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = var.ubuntu_min_2204
+      size  = var.disk_size_free
+      type  = var.disk_type_free
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,76 @@
+##     Secrets                  ##
+variable "project_name" {
+  type        = string
+  description = "Project Name"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Instance Name"
+}
+
+variable "credentials_file" {
+  type        = string
+  description = "JSON file from Service Account Key creation"
+  default     = "./creds.json"
+}
+
+
+##     Ubuntu Versions           ##
+
+variable "ubuntu_min_1804" {
+  type        = string
+  description = "Ubuntu Minimal - 18.04 - Bionic - LTS"
+  default     = "ubuntu-os-cloud/ubuntu-minimal-1804-lts"
+}
+
+variable "ubuntu_min_2004" {
+  type        = string
+  description = "Ubuntu Minimal - 20.04 - Focal - LTS"
+  default     = "ubuntu-os-cloud/ubuntu-minimal-2004-lts"
+}
+
+variable "ubuntu_min_2204" {
+  type        = string
+  description = "Ubuntu Minimal - 22.04 - Jammy - LTS"
+  default     = "ubuntu-os-cloud/ubuntu-minimal-2204-lts"
+}
+
+
+##      Free Tier               ##
+
+variable "machine_type_free" {
+  type        = string
+  description = "Free VM type"
+  default     = "e2-micro"
+}
+
+variable "zone_free_oregon" {
+  type        = string
+  description = "Free Zone Oregon"
+  default     = "us-west1-c"
+}
+
+variable "zone_free_iowa" {
+  type        = string
+  description = "Free Zone Iowa"
+  default     = "us-central1-c"
+}
+
+variable "zone_free_south_carolina" {
+  type        = string
+  description = "Free Zone South Carolina"
+  default     = "us-east1-c"
+}
+
+variable "disk_size_free" {
+  type        = number
+  description = "Maximum allowed hard drive space in GB"
+  default     = 30
+}
+
+variable "disk_type_free" {
+  type        = string
+  description = "Allowed type of persistence"
+  default     = "pd-standard"
+}
